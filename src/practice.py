8e262e4bae7644845dcb0771225155b3df6c6210
Thabani.M <macingwane413@gmail.com>
2026-08-31 07:06:11 +0200

    Plotting confusion matrix graph

--- src/practice.py
+++ src/practice.py
@@ -1,11 +1,11 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, classification_report,ConfusionMatrixDisplay,confusion_matrix
 import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
 
 
-df = pd.read_csv('../Dataset/equipment_anomaly_data.csv')
+df = pd.read_csv('Dataset/equipment_anomaly_data.csv')
 
 df['equipment'] = df['equipment'].map({'Turbine': 0, 'Compressor': 1, 'Pump': 2})
 df['location'] = df['location'].map({'Atlanta': 0, 'Chicago': 1, 'San Francisco': 2, 'New York': 3, 'Houston': 4})
@@ -18,8 +18,8 @@ features =['equipment',
 
 X = df[features]
 
-X.hist(bins=42, figsize=(12, 8))
-plt.show()
+#X.hist(bins=42, figsize=(12, 8))
+#plt.show()
 
 y = df['faulty']
 
@@ -37,5 +37,13 @@ accuracy = accuracy_score(
   y_test,
   prediction  
 )
+cm = confusion_matrix(y_test, prediction)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+disp.plot()
 
-print(accuracy)
+plt.title('Confusion Matrix')
+plt.show()
+classification_rep = classification_report(y_test, prediction)
+
+#print(accuracy)
+#print(classification_rep)
